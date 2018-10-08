c05c7843cdbff2ac289d21db4d41fcd049debdb3
Zuip <zuiohi@gmail.com>
2018-10-08 15:12:59 +0300

    Defined which columns are returned from the users database select

--- database/selectUsers.js
+++ database/selectUsers.js
@@ -3,13 +3,13 @@ let db = require('./connection');
 module.exports = {
   all() {
     return db.any(`
-      SELECT *
+      SELECT id, name, language_id, created
       FROM user_account
     `);
   },
   withIds(userIds) {
     return db.any(`
-      SELECT *
+      SELECT id, name, language_id, created
       FROM user_account
       WHERE user_account.id IN ($1:csv)
     `,
